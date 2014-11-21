4cc7d83df98def080c9ebb0bef0f3cd578d4bd11
Tom Briden <tom@decompile.me.uk>
2014-11-21 15:38:58 +0000

    AospPlus-ify

--- ax_shamu.mk
+++ ax_shamu.mk
@@ -17,23 +17,27 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
+$(call inherit-product, vendor/ax/config/gsm.mk)
+
+# Inherit some common AX stuff.
+$(call inherit-product, vendor/ax/config/common_full_phone.mk)
+
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_shamu
+PRODUCT_NAME := ax_shamu
 PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Shamu
+PRODUCT_MODEL := AOSP+ on Shamu
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/moto/shamu/device.mk)
-$(call inherit-product-if-exists, vendor/moto/shamu/device-vendor.mk)
+$(call inherit-product, vendor/moto/shamu/device-vendor.mk)
 
-PRODUCT_NAME := aosp_shamu
+PRODUCT_NAME := ax_shamu
 
 PRODUCT_PACKAGES += \
     Launcher3
